28ee2cff9c43c12ffb9082447c5de6a846a577a2
raymondcwh <1155126987@link.cuhk.edu.hk>
2023-03-20 23:27:22 +0800

    access personal hand histories files

---
+++ src/database.py
@@ -0,0 +1,10 @@
+import os
+from private.paths import *
+
+def GetAllCGLevel():
+    levels = os.listdir(HAND_HISTORY_FOLDER)
+    return levels
+
+def GetAllPeriods(level):
+    periods = os.listdir(os.path.join(HAND_HISTORY_FOLDER, level))
+    return periods
